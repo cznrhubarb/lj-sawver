332d3179a21879e2ff55278f63b1fec9fd419414
Mike <cznrhubarb@gmail.com>
2018-02-16 09:31:16 -0500

    Update production configuration take 3?

--- config/prod.exs
+++ config/prod.exs
@@ -19,7 +19,7 @@ config :sawver, SawverWeb.Endpoint,
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
-config :platform, Platform.Repo,
+config :sawver, Sawver.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
