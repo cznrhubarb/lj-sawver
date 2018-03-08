812d35e8dabc281764527af25408fb149ef426b3
Mike <cznrhubarb@gmail.com>
2018-03-07 21:45:23 -0500

    Forgot a comma

--- config/prod.exs
+++ config/prod.exs
@@ -23,7 +23,7 @@ config :sawver, SawverWeb.Endpoint,
 config :sawver, Sawver.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
-  pool_size: 1
+  pool_size: 1,
   #pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
   ssl: true
 
